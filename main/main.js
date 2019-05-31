a3e43ecef215843b99d93c3bc8250718195d4265
xff <572583146@qq.com>
2019-05-31 21:46:11 +0800

    add main

--- main/main.js
+++ main/main.js
@@ -1,53 +1,71 @@
-module.exports = function main(x) {
-    str = x;
-	out_str1 = '';
-	out_str2 = '';
-	out_str3 = '';
-	for (i = 0; i < str.length;i++){
-	  if (str[i] == 1) {
-		out_str1 += '... ';
-	  } else if (str[i] == 4) {
-		out_str1 += '... ';
-	  } else {
-		out_str1 += '._. ';
-	  }
-	  
-	  if (str[i] == 0) {
-		out_str2 += '|.| ';
-	  }else if (str[i] == 1){
-		out_str2 += '..| ';
-	  }else if (str[i] == 7){
-		out_str2 += '..| ';
-	  }else if (str[i] == 2){
-		out_str2 += '._| ';
-	  }else if (str[i] == 3){
-		out_str2 += '._| ';
-	  }else if (str[i] == 5){
-		out_str2 += '|_. ';
-	  }else if (str[i] == 6){
-		out_str2 += '|_. ';
-	  } else {
-		out_str2 += '|_| ';
-	  }		
-	  if (str[i] == 6){
-		out_str3 += '|_| ';
-	  }else if (str[i] == 8){
-		out_str3 += '|_| ';		  
-	  }else if (str[i] == 0){
-		out_str3 += '|_| ';
-	  }else if (str[i] == 2){
-		out_str3 += '|_. ';
-	  }else if (str[i] == 3){
-		out_str3 += '._| ';
-	  }else if (str[i] == 5){
-		out_str3 += '._| ';
-	  }else {
-		out_str3 += '..| ';
-	  }
-	}
-	out_str1 = out_str1.slice(0,-1) + '\n';
-	out_str2 = out_str2.slice(0,-1) + '\n';
-	out_str3 = out_str3.slice(0,-1) + '\n';
-	out_str = out_str1 + out_str2 + out_str3;
-	console.log(out_str);
-};
+var zero=new Array( "._.",
+			        "|.|",
+			        "|_|");
+var one =new Array( "...",
+			        "..|",
+			        "..|");
+var two =new Array(" ._.",
+			        "._|",
+			        "|_.");
+var three =new Array("._.",
+			         "._|",
+			         "._|");
+var four =new Array( "...",
+			         "|_|",
+			         "..|");
+var five =new Array( "._.",
+			         "|_.",
+			         "._|");
+var six = new Array(  "._.",
+			          "|_.",
+			          "|_|");
+var seven = new Array( "._.",
+			           "..|",
+			           "..|");	
+var eight = new Array("._.",
+			          "|_|",
+			          "|_|");
+var nine = new Array( "._.",
+			          "|_|",
+			          "..|");
+
+function myFunction(y) {
+  switch (y) {
+	  case 0:
+		print(zero);
+		break;
+	  case 1:
+		print(one);
+		break;
+	  case 2:
+		print(two);
+		break;
+	  case 3:
+		print(three);
+		break;
+	  case 4:
+	   print(four);
+		break;
+	  case 5:
+	   print(five);
+		break;
+	  case  6:
+		print(six);
+		break;
+	  case  7:
+	  print(seven);
+	  break;
+	  case  8:
+	  print(eight);
+	  break;
+	  case  9:
+	  print(nine);
+	  break;
+}
+}
+
+function print( number){
+	console.log(number[0]+'/n'+number[1]+'/n'+number[2]+' ')
+}
+
+myFunction(9)

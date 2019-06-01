7d0816555f588df51c75e3d31dd510810a1605bd
xff <572583146@qq.com>
2019-06-01 17:34:46 +0800

    add main

--- main/main.js
+++ main/main.js
@@ -1,85 +1,54 @@
-module.exports = function main(num) {
-var zero=new Array( "._.",
-			        "|.|",
-			        "|_|");
-var one =new Array( "...",
-			        "..|",
-			        "..|");
-var two =new Array("._.",
-			        "._|",
-			        "|_.");
-var three =new Array("._.",
-			         "._|",
-			         "._|");
-var four =new Array( "...",
-			         "|_|",
-			         "..|");
-var five =new Array( "._.",
-			         "|_.",
-			         "._|");
-var six = new Array(  "._.",
-			          "|_.",
-			          "|_|");
-var seven = new Array( "._.",
-			           "..|",
-			           "..|");	
-var eight = new Array("._.",
-			          "|_|",
-			          "|_|");
-var nine = new Array( "._.",
-			          "|_|",
-			          "..|");
-var a = new Array();
-function myFunction(y) {
-  switch (y) {
-	  case '0':
-		a.push(zero);
-		break;
-	  case '1':
-		a.push(one);
-		break;
-	  case '2':
-		a.push(two);
-		break;
-	  case '3':
-		a.push(three);
-		break;
-	  case '4':
-	   a.push(four);
-		break;
-	  case '5':
-	   a.push(five);
-		break;
-	  case  '6':
-		a.push(six);
-		break;
-	  case  '7':
-	  a.push(seven);
-	  break;
-	  case  '8':
-	  a.push(eight);
-	  break;
-	  case  '9':
-	  a.push(nine);
-	  break;
-}
-}
-var l = num.length;
-for(var i = 0 ; i < l;i++){
-	myFunction(num.charAt(i));
-}
-var first = new Array();
-var second = new Array();
-var third = new Array();
-for(var j = 0; j < l; j++) {
-first.push(a[j][0]);
-second.push(a[j][1]);
-third.push(a[j][2]);
-}
-first.join(' ');
-second.join(' ');
-third.join(' ');
-var str = first.join(' ')+'\n' + second.join(' ')+'\n' + third.join(' ') + '\n';
-return str;
-
+module.exports = function main(x) {
+    str = x;
+	out_str1 = '';
+	out_str2 = '';
+	out_str3 = '';
+	for (i = 0; i < str.length;i++){
+	  if (str[i] == 1) {
+		out_str1 += '... ';
+	  } else if (str[i] == 4) {
+		out_str1 += '... ';
+	  } else {
+		out_str1 += '._. ';
+	  }
+	  
+	  if (str[i] == 0) {
+		out_str2 += '|.| ';
+	  }else if (str[i] == 1){
+		out_str2 += '..| ';
+	  }else if (str[i] == 7){
+		out_str2 += '..| ';
+	  }else if (str[i] == 2){
+		out_str2 += '._| ';
+	  }else if (str[i] == 3){
+		out_str2 += '._| ';
+	  }else if (str[i] == 5){
+		out_str2 += '|_. ';
+	  }else if (str[i] == 6){
+		out_str2 += '|_. ';
+	  } else {
+		out_str2 += '|_| ';
+	  }		
+	  if (str[i] == 6){
+		out_str3 += '|_| ';
+	  }else if (str[i] == 8){
+		out_str3 += '|_| ';		  
+	  }else if (str[i] == 0){
+		out_str3 += '|_| ';
+	  }else if (str[i] == 2){
+		out_str3 += '|_. ';
+	  }else if (str[i] == 3){
+		out_str3 += '._| ';
+	  }else if (str[i] == 5){
+		out_str3 += '._| ';
+	  }else {
+		out_str3 += '..| ';
+	  }
+	}
+	out_str1 = out_str1.slice(0,-1) + '\n';
+	out_str2 = out_str2.slice(0,-1) + '\n';
+	out_str3 = out_str3.slice(0,-1) + '\n';
+	out_str = out_str1 + out_str2 + out_str3;
+	console.log(out_str);
+	return 'Hello World!';
 };
